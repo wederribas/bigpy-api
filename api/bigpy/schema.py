1cf24296934c3873e665468c5a0c7b5901f329e9
wederribas <wederribas@gmail.com>
2018-09-18 22:42:27 -0300

    Add query for company reports with its filters

--- api/bigpy/schema.py
+++ api/bigpy/schema.py
@@ -3,7 +3,7 @@ import django_filters
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .models import CustomerReports
+from .models import CustomerReports, CompanyReports
 
 
 class CustomerReportsFilter(django_filters.FilterSet):
@@ -18,7 +18,24 @@ class CustomerReportsType(DjangoObjectType):
         interfaces = (graphene.relay.Node, )
 
 
+class CompanyReportsFilter(django_filters.FilterSet):
+    class Meta:
+        model = CompanyReports
+        fields = ['region', 'state', 'city',
+                  'gender', 'age_range', 'company_name']
+
+
+class CompanyReportsType(DjangoObjectType):
+    class Meta:
+        model = CompanyReports
+        interfaces = (graphene.relay.Node, )
+
+
 class Query(graphene.ObjectType):
     customer_report = graphene.relay.Node.Field(CustomerReportsType)
     all_customer_reports = DjangoFilterConnectionField(
         CustomerReportsType, filterset_class=CustomerReportsFilter)
+
+    company_report = graphene.relay.Node.Field(CompanyReportsType)
+    all_company_reports = DjangoFilterConnectionField(
+        CompanyReportsType, filterset_class=CompanyReportsFilter)
