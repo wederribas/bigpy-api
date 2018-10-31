797fa26ef1a0934f109d23d3238539e9171444a2
wederribas <wederribas@gmail.com>
2018-10-30 23:55:33 -0300

    Implement totalCount and countFiltered fields to get reports count

--- api/bigpy/schema.py
+++ api/bigpy/schema.py
@@ -18,6 +18,20 @@ class CustomerReportsType(DjangoObjectType):
         interfaces = (graphene.relay.Node, )
 
 
+class CompanyReportsConnection(graphene.relay.Connection):
+    class Meta:
+        abstract = True
+
+    total_count = graphene.Int()
+    count_filtered = graphene.Int()
+
+    def resolve_total_count(self, info, **kwargs):
+        return CompanyReports.objects.mongo_count()
+
+    def resolve_count_filtered(self, info, **kwargs):
+        return self.iterable.count()
+
+
 class CompanyReportsFilter(django_filters.FilterSet):
     class Meta:
         model = CompanyReports
@@ -29,6 +43,7 @@ class CompanyReportsType(DjangoObjectType):
     class Meta:
         model = CompanyReports
         interfaces = (graphene.relay.Node, )
+        connection_class = CompanyReportsConnection
 
 
 class Query(graphene.ObjectType):
