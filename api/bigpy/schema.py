d81930f65f989bf77d907630e299780d8c12b448
wederribas <wederribas@gmail.com>
2018-11-10 16:10:32 -0200

    Add new API endpoints to fill data charts

--- api/bigpy/schema.py
+++ api/bigpy/schema.py
@@ -5,7 +5,7 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import CustomerReports, CompanyReports
-from .utils import parse_results_as_graphe_object
+from .utils import parse_as_graphene_object
 
 
 class CustomerReportsFilter(django_filters.FilterSet):
@@ -43,6 +43,26 @@ class CompanyReportsCount(graphene.ObjectType):
     count = graphene.String()
 
 
+class CompanyReportsPerRegion(graphene.ObjectType):
+    region = graphene.String()
+    count = graphene.String()
+
+
+class CompanyReportsPerAgeRange(graphene.ObjectType):
+    age_range = graphene.String()
+    count = graphene.String()
+
+
+class CompanyRatingPerYear(graphene.ObjectType):
+    year = graphene.String()
+    average = graphene.String()
+
+
+class CompanyReportsPerGender(graphene.ObjectType):
+    gender = graphene.String()
+    count = graphene.String()
+
+
 class Query(graphene.ObjectType):
     customer_report = graphene.relay.Node.Field(CustomerReportsType)
     all_customer_reports = DjangoFilterConnectionField(
@@ -53,13 +73,36 @@ class Query(graphene.ObjectType):
         CompanyReportsType, filterset_class=CompanyReportsFilter)
 
     companies_names = graphene.List(graphene.String)
+
     total_company_reports = graphene.Int(company_name=graphene.String())
+
     total_company_replies = graphene.Int(company_name=graphene.String())
+
     overall_company_rating = graphene.Float(company_name=graphene.String())
+
     reports_per_date = graphene.List(
         CompanyReportsPerDate,
         company_name=graphene.String())
+
     companies_reports_count = graphene.List(CompanyReportsCount)
+
+    company_reports_per_region = graphene.List(
+        CompanyReportsPerRegion,
+        company_name=graphene.String())
+
+    company_reports_per_age = graphene.List(
+        CompanyReportsPerAgeRange,
+        company_name=graphene.String())
+
+    company_rating_per_year = graphene.Int(company_name=graphene.String())
+
+    company_rating_per_year = graphene.List(
+        CompanyRatingPerYear,
+        company_name=graphene.String())
+
+    company_reports_per_gender = graphene.List(
+        CompanyReportsPerGender,
+        company_name=graphene.String())
 
     def resolve_companies_names(self, info, **kwargs):
         return CompanyReports.objects.mongo_distinct('company_name')
@@ -109,9 +152,10 @@ class Query(graphene.ObjectType):
             {'$sort': {'year': -1}}
         ])]
 
-        return parse_results_as_graphe_object(
-            results,
+        return parse_as_graphene_object(
             'year',
+            'count',
+            results,
             CompanyReportsPerDate
         )
 
@@ -129,8 +173,109 @@ class Query(graphene.ObjectType):
             {'$limit': 10}
         ])]
 
-        return parse_results_as_graphe_object(
-            results,
+        return parse_as_graphene_object(
             'company_name',
+            'count',
+            results,
             CompanyReportsCount
+        )
+
+    def resolve_company_reports_per_region(self, info, company_name, **kwargs):
+        results = [i for i in CompanyReports.objects.mongo_aggregate([
+            {
+                '$match': {
+                    'company_name': company_name
+                }
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'region': '$region'
+                    },
+                    'count': {'$sum': 1}
+                }
+            },
+            {'$sort': {'count': -1}}
+        ])]
+
+        return parse_as_graphene_object(
+            'region',
+            'count',
+            results,
+            CompanyReportsPerRegion
+        )
+
+    def resolve_company_reports_per_age(self, info, company_name, **kwargs):
+        results = [i for i in CompanyReports.objects.mongo_aggregate([
+            {
+                '$match': {
+                    'company_name': company_name
+                }
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'age_range': '$age_range'
+                    },
+                    'count': {'$sum': 1}
+                }
+            },
+            {'$sort': {'count': -1}}
+        ])]
+
+        return parse_as_graphene_object(
+            'age_range',
+            'count',
+            results,
+            CompanyReportsPerAgeRange
+        )
+
+    def resolve_company_rating_per_year(self, info, company_name, **kwargs):
+        results = [i for i in CompanyReports.objects.mongo_aggregate([
+            {
+                '$match': {
+                    'company_name': company_name
+                }
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'year': {'$year': '$conclusion_date'},
+                    },
+                    'average': {'$avg': '$customer_rating'}
+                }
+            },
+            {'$sort': {'year': -1}}
+        ])]
+
+        return parse_as_graphene_object(
+            'year',
+            'average',
+            results,
+            CompanyRatingPerYear
+        )
+
+    def resolve_company_reports_per_gender(self, info, company_name, **kwargs):
+        results = [i for i in CompanyReports.objects.mongo_aggregate([
+            {
+                '$match': {
+                    'company_name': company_name
+                }
+            },
+            {
+                '$group': {
+                    '_id': {
+                        'gender': '$gender',
+                    },
+                    'count': {'$sum': 1}
+                }
+            },
+            {'$sort': {'gender': -1}}
+        ])]
+
+        return parse_as_graphene_object(
+            'gender',
+            'count',
+            results,
+            CompanyReportsPerGender
         )
